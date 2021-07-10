8ac56786f1deeb84bf338ceccb453a20ae5fea15
ddamss <46570583+ddamss@users.noreply.github.com>
2021-07-10 19:52:34 +0400

    remove the test view shown instead of the actual display_forecat_data

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/cashback', function () {
-    // return view('display_forecast_data');
-    dd("hello");
+    return view('display_forecast_data');
 });
